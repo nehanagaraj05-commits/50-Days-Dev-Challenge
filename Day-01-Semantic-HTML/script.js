b4947c1029be54661e6caa3ac9b57b57a586632d
nehanagaraj05-commits <nehanagaraj05@gmail.com>
2026-07-26 20:06:01 +0530

    Day 11: DOM fundamentals - hero click interaction

---
+++ Day-01-Semantic-HTML/script.js
@@ -0,0 +1,19 @@
+/* ========================================== */
+/* DAY 11: DOM FUNDAMENTALS & EVENTS          */
+/* ========================================== */
+
+console.log("Synexus Engine Initialized. Ready for logic.");
+
+// 1. DOM SELECTION
+const heroHeadline = document.querySelector('#home h1');
+const heroButton = document.querySelector('.btn-cta');
+
+// 2. EVENT LISTENER
+heroButton.addEventListener('click', function() {
+
+    // 3. DOM MANIPULATION
+    heroHeadline.textContent = 'Welcome to the Synexus Core!';
+
+    // Bonus Challenge
+    heroHeadline.classList.toggle('active-state');
+});
