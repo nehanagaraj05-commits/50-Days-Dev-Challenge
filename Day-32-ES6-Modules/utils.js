f9ada6bfecb79194fbcca394207b05756f1990a0
nehanagaraj05-commits <nehanagaraj05@gmail.com>
2026-08-24 09:14:25 +0530

    Day 32: ES6 module architecture - split into utils.js, api.js, main.js

--- Day-32-ES6-Modules/utils.js
+++ Day-32-ES6-Modules/utils.js
@@ -1,20 +1,11 @@
 /* ========================================== */
-/* utils.js: Helper Functions                 */
+/* utils.js: Shared Helper Functions          */
 /* ========================================== */
 
-// By adding 'export', we allow other files to use this function
 export function debounce(func, delay = 500) {
-    let timeoutId;
-    return function (...args) {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => {
-            func.apply(this, args);
-        }, delay);
-    };
-}
-
-// You can export as many things as you want from a single file
-export function formatDate(dateString) {
-    const options = { year: 'numeric', month: 'short', day: 'numeric' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
 }
