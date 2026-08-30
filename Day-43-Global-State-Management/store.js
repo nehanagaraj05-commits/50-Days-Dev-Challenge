8569db2443c30237b47e16184f50fc073be95fd0
nehanagaraj05-commits <nehanagaraj05@gmail.com>
2026-08-30 12:34:54 +0530

    Day 43: Pub/Sub global state store synced with theme toggle and live stats widget

--- Day-43-Global-State-Management/store.js
+++ Day-43-Global-State-Management/store.js
@@ -3,47 +3,33 @@
 /* ========================================== */
 
 class StateStore {
-    constructor(initialState = {}) {
-        this.state = initialState;
-        
-        // This array holds the callbacks of anyone who wants to know when data changes
-        this.listeners = []; 
-    }
+  constructor(initialState = {}) {
+    this.state = initialState;
+    this.listeners = [];
+  }
 
-    // 1. READ STATE
-    getState() {
-        return this.state;
-    }
+  getState() {
+    return this.state;
+  }
 
-    // 2. WRITE STATE & BROADCAST
-    setState(newStatePayload) {
-        // Merge the old state with the new changes
-        this.state = { ...this.state, ...newStatePayload };
-        
-        console.log("🔄 Global State Updated:", this.state);
+  setState(newStatePayload) {
+    this.state = { ...this.state, ...newStatePayload };
+    console.log("🔄 Global State Updated:", this.state);
+    this.listeners.forEach((listenerCallback) => {
+      listenerCallback(this.state);
+    });
+  }
 
-        // Notify every single subscriber that the data has changed
-        this.listeners.forEach((listenerCallback) => {
-            listenerCallback(this.state);
-        });
-    }
-
-    // 3. THE SUBSCRIPTION PIPELINE
-    subscribe(listenerCallback) {
-        // Add the component's callback to our broadcast list
-        this.listeners.push(listenerCallback);
-
-        // Return an unsubscribe function to prevent memory leaks!
-        return () => {
-            this.listeners = this.listeners.filter(l => l !== listenerCallback);
-        };
-    }
+  subscribe(listenerCallback) {
+    this.listeners.push(listenerCallback);
+    return () => {
+      this.listeners = this.listeners.filter((l) => l !== listenerCallback);
+    };
+  }
 }
 
-// 4. EXPORT A SINGLETON
-// We export ONE instance so the entire app shares the exact same memory bank
 export const globalStore = new StateStore({
-    cartCount: 0,
-    activeUser: null,
-    isDarkMode: false
+  cartCount: 0,
+  activeUser: null,
+  isDarkMode: false,
 });
